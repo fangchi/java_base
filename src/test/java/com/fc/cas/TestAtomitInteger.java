43103fefd5b0ebab8d0a01cbc3ce443fa3b05b83
chi.fang <chi.fang@china.zhaogang.com>
2019-03-13 13:32:22 +0800

    x

---
+++ src/test/java/com/fc/cas/TestAtomitInteger.java
@@ -0,0 +1,13 @@
+package com.fc.cas;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.Test;
+
+public class TestAtomitInteger {
+
+	@Test
+	public void testAutoMic() {
+		AtomicInteger atomicInteger = new AtomicInteger();
+	}
+}

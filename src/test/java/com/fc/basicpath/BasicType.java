61f2da72a737755979f3a331b5834ad482bcf010
fangchi <fangchi1986@aliyun.com>
2019-05-28 10:58:46 +0800

    add

--- src/test/java/com/fc/basicpath/BasicType.java
+++ src/test/java/com/fc/basicpath/BasicType.java
@@ -5,17 +5,16 @@ package com.fc.basicpath;
  */
 public class BasicType {
 
-	public static void main(String[] args) {
-		  
-		Integer i1 = 100;
+    public static void main(String[] args) {
+
+        Integer i1 = 100;
         Integer i2 = 100;
         Integer i3 = 200;
         Integer i4 = 200;
-         
-        System.out.println(i1==i2);
-        System.out.println(i3==i4);
-        System.out.println(i3.intValue()==200);
-        System.out.println(i3==200);
-        System.out.println(i3==Integer.valueOf(200));
-	}
+        System.out.println(i1 == i2);
+        System.out.println(i3 == i4);
+        System.out.println(i3.intValue() == 200);
+        System.out.println(i3 == 200);
+        System.out.println(i3 == Integer.valueOf(200));
+    }
 }
